ed0db0eb6b04c095fc5e67186af16a8080acee0c
Robert Yee <Ryee002@ucr.edu>
2019-05-19 16:12:18 -0700

    fixed up new api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyBvkAwQ9WozNOpKZDbfM1ol2w9eadSwh7A';
+const KEY = 'AIzaSyBMxjR2X4WOIn6jgG5MFH_1g-4f5Jo0bQI';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
